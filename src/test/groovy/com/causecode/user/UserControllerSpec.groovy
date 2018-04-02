44f9ad003387a965f6fb6c6f501e0349e1bd895e
Ankit Agrawal <ankit.agrawal@causecode.com>
2018-04-02 16:07:10 +0530

    Minor changes.

--- src/test/groovy/com/causecode/user/UserControllerSpec.groovy
+++ src/test/groovy/com/causecode/user/UserControllerSpec.groovy
@@ -1,10 +1,3 @@
-/*
- * Copyright (c) 2017, CauseCode Technologies Pvt Ltd, India.
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or
- * without modification, are not permitted.
- */
 package com.causecode.user
 
 import com.causecode.SignUpNotAllowedException
